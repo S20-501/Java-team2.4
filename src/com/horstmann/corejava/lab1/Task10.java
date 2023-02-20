f2191275026b7e8ecc5f7626d4595bb161fc30a3
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-02-20 21:22:11 +0300

    String generates of user defined length

--- src/com/horstmann/corejava/lab1/Task10.java
+++ src/com/horstmann/corejava/lab1/Task10.java
@@ -1,9 +1,36 @@
 package com.horstmann.corejava.lab1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Task10 {
     public static void main(String[] args){
-        System.out.print(Long.toString(new Random().nextLong(Long.MAX_VALUE), 36));
+        System.out.print("Enter ceil number: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()){
+            int num = scanner.nextInt();
+
+            if (num <= 0) return;
+
+            long randNum;
+
+            long minNum = (long) Math.pow(36, num - 1);
+            long maxNum = (long) Math.pow(36, num);
+
+            if (maxNum == minNum){
+                System.err.println("Too high for long number");
+                return;
+            }
+
+            Random rnd = new Random();
+
+            randNum = rnd.nextLong(minNum, maxNum);
+
+            System.out.println(Long.toString(randNum, 36));
+        } else {
+            System.out.println("Number incorrect!");
+        }
     }
 }
