c43e2d4ebd54ab12cca03483da5e2c425b80d823
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-02-20 21:58:27 +0300

    String generates of user defined length, updated

--- src/com/horstmann/corejava/lab1/Task10.java
+++ src/com/horstmann/corejava/lab1/Task10.java
@@ -1,34 +1,45 @@
 package com.horstmann.corejava.lab1;
 
+import java.math.BigDecimal;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Task10 {
     public static void main(String[] args){
+        BigDecimal test;
+
         System.out.print("Enter ceil number: ");
 
         Scanner scanner = new Scanner(System.in);
 
         if (scanner.hasNextInt()){
-            int num = scanner.nextInt();
+            int num1 = scanner.nextInt();
 
-            if (num <= 0) return;
+            if (num1 <= 0) return;
 
             long randNum;
 
-            long minNum = (long) Math.pow(36, num - 1);
-            long maxNum = (long) Math.pow(36, num);
+            do {
+                int num = Math.min(num1, 10);
 
-            if (maxNum == minNum){
-                System.err.println("Too high for long number");
-                return;
-            }
+                long minNum = (long) Math.pow(36, num - 1);
+                long maxNum = (long) Math.pow(36, num);
 
-            Random rnd = new Random();
 
-            randNum = rnd.nextLong(minNum, maxNum);
+                if (maxNum == minNum){
+                    System.err.println("Too high for long number");
+                    return;
+                }
 
-            System.out.println(Long.toString(randNum, 36));
+                Random rnd = new Random();
+
+                randNum = rnd.nextLong(minNum, maxNum);
+
+                System.out.print(Long.toString(randNum, 36));
+
+                num1 -= num;
+            } while (num1 > 0);
+
         } else {
             System.out.println("Number incorrect!");
         }
