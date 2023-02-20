05b027c021492a9521e1017ced7bb4c11dbf0682
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-02-20 22:01:40 +0300

    String generates of user defined length, updated

--- src/com/horstmann/corejava/lab1/Task10.java
+++ src/com/horstmann/corejava/lab1/Task10.java
@@ -1,44 +1,34 @@
 package com.horstmann.corejava.lab1;
 
-import java.math.BigDecimal;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Task10 {
     public static void main(String[] args){
-        BigDecimal test;
-
         System.out.print("Enter ceil number: ");
 
         Scanner scanner = new Scanner(System.in);
 
         if (scanner.hasNextInt()){
-            int num1 = scanner.nextInt();
+            int charCount = scanner.nextInt();
 
-            if (num1 <= 0) return;
+            if (charCount <= 0) return;
 
             long randNum;
+            Random rnd = new Random();
 
             do {
-                int num = Math.min(num1, 10);
+                int chunkCount = Math.min(charCount, 10);
 
-                long minNum = (long) Math.pow(36, num - 1);
-                long maxNum = (long) Math.pow(36, num);
-
-
-                if (maxNum == minNum){
-                    System.err.println("Too high for long number");
-                    return;
-                }
-
-                Random rnd = new Random();
+                long minNum = (long) Math.pow(36, chunkCount - 1);
+                long maxNum = (long) Math.pow(36, chunkCount);
 
                 randNum = rnd.nextLong(minNum, maxNum);
 
                 System.out.print(Long.toString(randNum, 36));
 
-                num1 -= num;
-            } while (num1 > 0);
+                charCount -= chunkCount;
+            } while (charCount > 0);
 
         } else {
             System.out.println("Number incorrect!");
