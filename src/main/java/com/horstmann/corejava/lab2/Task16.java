6aa96f79218923864a5e838826074a07353a6c38
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-03-04 22:03:45 +0300

    Add `Queue` class

---
+++ src/main/java/com/horstmann/corejava/lab2/Task16.java
@@ -0,0 +1,60 @@
+package com.horstmann.corejava.lab2;
+
+public class Task16 {
+    static class Queue{
+        private static class Node{
+            private final String string;
+            private Node next = null;
+
+            public Node(String string){
+                this.string = string;
+            }
+        }
+
+        private Node head = null;
+
+        public Queue add(String str){
+            if (head == null) {
+                head = new Node(str);
+
+                return this;
+            }
+
+            Node iterator = head;
+
+            while (iterator.next != null) {
+                iterator = iterator.next;
+            }
+
+            iterator.next = new Node(str);
+
+            return this;
+        }
+
+        public String remove(){
+            if (head == null) {
+                return null;
+            }
+
+            String str = head.string;
+            head = head.next;
+
+            return str;
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        Queue q = new Queue();
+
+        q.add("test1").add("test2").add("test3");
+
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+
+}
