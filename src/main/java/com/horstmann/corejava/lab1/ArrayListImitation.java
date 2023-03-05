5566e6f60f00254593debfc47b55ab21983a2b37
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-03-05 22:40:52 +0300

    Add additional task of lab1

---
+++ src/main/java/com/horstmann/corejava/lab1/ArrayListImitation.java
@@ -0,0 +1,45 @@
+package com.horstmann.corejava.lab1;
+
+import java.util.Arrays;
+
+public class ArrayListImitation {
+    /**
+     * @author Nikita Sazikov
+     */
+    public static String[] addToTheEnd(String[] arr,long capacity,String el){
+
+        if (capacity == arr.length)
+            arr = grow((capacity+1),arr);
+        arr[(int) capacity] = el;
+        //  size = s + 1;
+        return arr;
+    }
+
+    /**
+     * @author Nikita Sazikov
+     */
+    private static String[] grow(long minCapacity, String[] arr) {
+        int oldCapacity = arr.length;
+        int newCapacity = (int) ((oldCapacity) + Math.max((minCapacity - oldCapacity), oldCapacity >> 1));
+        return Arrays.copyOf(arr, newCapacity);
+    }
+
+    /**
+     * @author Evgeny Ezers
+     */
+    public static void main(String[] args){
+        String[] arr = new String[8];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(i);
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr.length);
+
+        String[] newArr = addToTheEnd(arr, arr.length, "test");
+
+        System.out.println(Arrays.toString(newArr));
+        System.out.println(newArr.length);
+    }
+}
