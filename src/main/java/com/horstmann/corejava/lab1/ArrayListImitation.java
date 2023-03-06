0b4ea7b88e9a99574d02d83ed5e08a9c7bde0097
JustBypass <71075649+JustBypass@users.noreply.github.com>
2023-03-06 17:36:13 +0300

    Fixed OutOfBoundException case

--- src/main/java/com/horstmann/corejava/lab1/ArrayListImitation.java
+++ src/main/java/com/horstmann/corejava/lab1/ArrayListImitation.java
@@ -3,33 +3,7 @@ package com.horstmann.corejava.lab1;
 import java.util.Arrays;
 
 public class ArrayListImitation {
-    /**
-     * @author Nikita Sazikov
-     */
-    public static String[] addToTheEnd(String[] arr,long capacity,String el){
-
-        if (capacity == arr.length)
-            arr = grow((capacity+1),arr);
-
-
-        arr[(int) capacity] = el;
-        //  size = s + 1;
-        return arr;
-    }
-
-    /**
-     * @author Nikita Sazikov
-     */
-    private static String[] grow(long minCapacity, String[] arr) {
-        int oldCapacity = arr.length;
-        int newCapacity = (int) ((oldCapacity) + Math.max((minCapacity - oldCapacity), oldCapacity >> 1));
-        return Arrays.copyOf(arr, newCapacity);
-    }
-
-    /**
-     * @author Evgeny Ezers
-     */
-    public static void main(String[] args){
+     public static void main(String[] args) {
         String[] arr = new String[8];
 
         for (int i = 0; i < arr.length; i++) {
@@ -43,10 +17,19 @@ public class ArrayListImitation {
 
         System.out.println(Arrays.toString(newArr));
         System.out.println(newArr.length);
+    }
 
-        String[] newArr2 = addToTheEnd(arr, arr.length, "test2");
+    public static String[] addToTheEnd(String[] arr,long capacity,String el){
 
-        System.out.println(Arrays.toString(newArr2));
-        System.out.println(newArr2.length);
+        if (capacity+1 == arr.length)
+            arr = grow((capacity+1),arr);
+        arr[(int) capacity] = el;
+        return arr;
+    }
+
+    private static String[] grow(long minCapacity, String[] arr) {
+        int oldCapacity = arr.length;
+        int newCapacity = (int) ((oldCapacity) + Math.max((minCapacity - oldCapacity), oldCapacity >> 1));
+        return arr = Arrays.copyOf(arr, newCapacity);
     }
 }
