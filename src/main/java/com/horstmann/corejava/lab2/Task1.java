324db01303f818a69402297212c6aac6a2c9afc7
Gmakk <gleb21410@yandex.ru>
2023-04-03 00:32:41 +0300

    Task1/9

--- src/main/java/com/horstmann/corejava/lab2/Task1.java
+++ src/main/java/com/horstmann/corejava/lab2/Task1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        String[] days = {"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
         System.out.print("Enter start day of week:");
         Scanner scanner = new Scanner(System.in);
         int numberOfDay = scanner.nextInt();
@@ -13,16 +14,25 @@ public class Task1 {
             System.out.println("Incorrect day of week");
             return;
         }
-
         //берем начало года
         LocalDate date = LocalDate.of(2023, 1, 1);
-        for(int i=1; i<=12; i++) {
+        for(int i=0; i<12; i++) {
             //отступ в начале месяца
             int value =date.getDayOfWeek().getValue();
 
             int shift = value - numberOfDay;
             if(shift<0)
                 shift = 7 + shift;
+
+            //дни недели
+            if(i == 0){
+                System.out.print("  ");
+                for (int k=numberOfDay-1;k<=6;k++)
+                    System.out.print(days[k] + "  ");
+                for (int k=0;k<numberOfDay-1;k++)
+                    System.out.print(days[k] + "  ");
+                System.out.print("\n");
+            }
 
             for (int j = 0; j < shift; j++)
                 System.out.print("\t");
--- src/main/java/com/horstmann/corejava/lab2/Task9.java
+++ src/main/java/com/horstmann/corejava/lab2/Task9.java
@@ -1,17 +1,23 @@
 package com.horstmann.corejava.lab2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Task9 {
     public static class car{
         private final float fuelVolume;
         private float fuelRemain = 0;
         private float fuelConsumption;
-        private float axisPoint = 0;
+        private float XaxisPoint = 0;
+        private float YaxisPoint = 0;
         private float totalDistance=0;
         public car(float volume, float consumption){
             fuelVolume = volume;
             fuelConsumption = consumption;
         }
 
-        public void drive(float km,int direction){// 0 -- left   not 0 -- right
+        public void drive(float km,float direction){// direction - angle
             if(km < 0) {
                 System.out.println("Incorrect distance");
                 return;
@@ -22,12 +28,38 @@ public class Task9 {
             }
 
             fuelRemain -= fuelConsumption*km;
-            if(direction == 0)
-                axisPoint -= km;
-            else
-                axisPoint += km;
+
+            XaxisPoint+=km*Math.cos(Math.toRadians(direction));
+            YaxisPoint+=km*Math.sin(Math.toRadians(direction));
 
             totalDistance+=km;
+        }
+
+        public void returnHome(){
+            float distance = (float)Math.sqrt(XaxisPoint*XaxisPoint+YaxisPoint*YaxisPoint);
+            float arccos = (float)Math.toDegrees(Math.acos(XaxisPoint/distance));
+            float arcsin = (float)Math.toDegrees(Math.asin(YaxisPoint/distance));
+            float drive_dist;
+
+            if(fuelRemain < fuelConsumption*distance){
+                drive_dist = fuelRemain/fuelConsumption;//если топлива не хватает, едем сколько можем
+            }else{
+                drive_dist = distance;//если хватает, едем до конца
+            }
+
+            if(arcsin>0){
+                if(arccos>0){//1
+                    drive(drive_dist,arccos-180);
+                }else{//2
+                    drive(drive_dist,arccos);
+                }
+            }else{
+                if(arccos<0){//3
+                    drive(drive_dist,-arcsin);
+                }else{//4
+                    drive(drive_dist,180+arcsin);
+                }
+            }
         }
 
         public void refuel(float liters){
@@ -46,19 +78,55 @@ public class Task9 {
         public float getTotalDistance(){
             return totalDistance;
         }
-        public float getAxisPoint(){return axisPoint;}
+        public float getXAxisPoint(){return XaxisPoint;}
+        public float getYAxisPoint(){return YaxisPoint;}
         public float getFuelRemain(){
             return fuelRemain;
         }
     }
-    public static void main(String[] args){
-        car regularCar = new car(100,1);
-        regularCar.refuel(50);
-        regularCar.refuel(60);
-        regularCar.drive(60,0);
-        regularCar.drive(50,1);
-        System.out.println("Total distance: "+ regularCar.getTotalDistance());
-        System.out.println("Fuel remains: "+ regularCar.getFuelRemain());
-        System.out.println("Axis point: "+ regularCar.getAxisPoint());
+    public static void main(String[] args) throws Exception{
+        String menu = "1.drive\n2.refuel\n3.get home\n4.info\n5.exit\n";
+        //создание машины
+        System.out.print("Enter volume and consumption:");
+        Scanner scanner = new Scanner(System.in);
+        float volume = scanner.nextInt();
+        float consumption = scanner.nextInt();
+        car regularCar = new car(volume,consumption);// 100 1
+
+
+        float km,direction;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while (true) {
+            System.out.print(menu);
+            s = reader.readLine();
+            if (s.equals("5"))
+                break;
+            if (s.equals("1")) {
+                System.out.println("Enter km and direction:");
+                km = scanner.nextInt();
+                direction = scanner.nextInt();
+                regularCar.drive(km,direction);
+                continue;
+            }
+            if (s.equals("2")) {
+                volume = scanner.nextInt();
+                regularCar.refuel(volume);
+                continue;
+            }
+            if (s.equals("3")) {
+                regularCar.returnHome();
+                continue;
+            }
+            if (s.equals("4")) {
+                System.out.println("Total distance: "+ regularCar.getTotalDistance());
+                System.out.println("Fuel remains: "+ regularCar.getFuelRemain());
+                System.out.println("X Axis point: "+ regularCar.getXAxisPoint());
+                System.out.println("Y Axis point: "+ regularCar.getYAxisPoint());
+                continue;
+            }
+            System.out.print("Wrong input");
+        }
     }
 }
