59745c1d9725eca143342d4b077a593ce062c07f
Evgeny Ezers <30502462+evezers@users.noreply.github.com>
2023-04-17 20:24:25 +0300

    Use MyComparator type

--- src/main/java/com/horstmann/corejava/lab3/MyComparator/MyComparator.java
+++ src/main/java/com/horstmann/corejava/lab3/MyComparator/MyComparator.java
@@ -9,13 +9,12 @@ import java.util.function.ToLongFunction;
 
 public interface MyComparator<T> extends Comparator<T> {
     int compare(T first, T second);
-    @Override
-    default Comparator<T> reversed() {
+
+    default MyComparator<T> reversed() {
         return (first, second) -> compare(second, first);
     }
 
-    @Override
-    default Comparator<T> thenComparing(Comparator<? super T> other) {
+    default MyComparator<T> thenComparing(MyComparator<? super T> other) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -26,8 +25,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    @Override
-    default <U> Comparator<T> thenComparing(Function<? super T, ? extends U> extractor, Comparator<? super U> comparator) {
+    default <U> MyComparator<T> thenComparing(Function<? super T, ? extends U> extractor, MyComparator<? super U> comparator) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -38,8 +36,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    @Override
-    default <U extends Comparable<? super U>> Comparator<T> thenComparing(Function<? super T, ? extends U> extractor) {
+    default <U extends Comparable<? super U>> MyComparator<T> thenComparing(Function<? super T, ? extends U> extractor) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -50,8 +47,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    @Override
-    default Comparator<T> thenComparingInt(ToIntFunction<? super T> extractor) {
+    default MyComparator<T> thenComparingInt(ToIntFunction<? super T> extractor) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -62,8 +58,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    @Override
-    default Comparator<T> thenComparingLong(ToLongFunction<? super T> extractor) {
+    default MyComparator<T> thenComparingLong(ToLongFunction<? super T> extractor) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -74,8 +69,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    @Override
-    default Comparator<T> thenComparingDouble(ToDoubleFunction<? super T> extractor) {
+    default MyComparator<T> thenComparingDouble(ToDoubleFunction<? super T> extractor) {
         return (first, second) -> {
             int result = compare(first, second);
             if(result == 0){
@@ -86,15 +80,15 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    static <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
+    static <T extends Comparable<? super T>> MyComparator<T> reverseOrder() {
         return (first, second) -> second.compareTo(first);
     }
 
-    static <T extends Comparable<? super T>> Comparator<T> naturalOrder() {
+    static <T extends Comparable<? super T>> MyComparator<T> naturalOrder() {
         return (first, second) -> first.compareTo(second);
     }
 
-    static <T> Comparator<T> nullsFirst(Comparator<? super T> comparator) {
+    static <T> MyComparator<T> nullsFirst(MyComparator<? super T> comparator) {
         return (first, second) -> {
             if(first == null) return -1;
             if(second == null) return 1;
@@ -102,7 +96,7 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    static <T> Comparator<T> nullsLast(Comparator<? super T> comparator) {
+    static <T> MyComparator<T> nullsLast(MyComparator<? super T> comparator) {
         return (first, second) -> {
             if(first == null) return 1;
             if(second == null) return -1;
@@ -110,23 +104,23 @@ public interface MyComparator<T> extends Comparator<T> {
         };
     }
 
-    static <T, U> Comparator<T> comparing(Function<? super T, ? extends U> extractor, Comparator<? super U> comparator) {
+    static <T, U> MyComparator<T> comparing(Function<? super T, ? extends U> extractor, MyComparator<? super U> comparator) {
         return (first, second) -> comparator.compare(extractor.apply(first), extractor.apply(second));
     }
 
-    static <T, U extends Comparable<? super U>> Comparator<T> comparing(Function<? super T, ? extends U> extractor) {
+    static <T, U extends Comparable<? super U>> MyComparator<T> comparing(Function<? super T, ? extends U> extractor) {
         return (first, second) -> extractor.apply(first).compareTo(extractor.apply(second));
     }
 
-    static <T> Comparator<T> comparingInt(ToIntFunction<? super T> extractor) {
+    static <T> MyComparator<T> comparingInt(ToIntFunction<? super T> extractor) {
         return (first, second) -> Integer.compare(extractor.applyAsInt(first), extractor.applyAsInt(second));
     }
 
-    static <T> Comparator<T> comparingLong(ToLongFunction<? super T> extractor) {
+    static <T> MyComparator<T> comparingLong(ToLongFunction<? super T> extractor) {
         return (first, second) -> Long.compare(extractor.applyAsLong(first), extractor.applyAsLong(second));
     }
 
-    static <T> Comparator<T> comparingDouble(ToDoubleFunction<? super T> extractor) {
+    static <T> MyComparator<T> comparingDouble(ToDoubleFunction<? super T> extractor) {
         return (first, second) -> Double.compare(extractor.applyAsDouble(first), extractor.applyAsDouble(second));
     }
 }
